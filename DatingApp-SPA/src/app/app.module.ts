eed65d9bd59c9c305e1b080df482fc1e64fed7d2
datngohavan <ngohavandat93ndc@gmail.com>
2019-04-09 00:28:56 +0700

    angular service

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -1,23 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+import { AuthService } from './_services/auth.service';
+
 
 @NgModule({
    declarations: [
       AppComponent,
-      ValueComponent
+      ValueComponent,
+      NavComponent,
+      NavComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
-   bootstrap: [
-      AppComponent
-   ]
+   providers: [
+      AuthService
+   ],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- DatingApp.API/Data/AuthRepository.cs
+++ DatingApp.API/Data/AuthRepository.cs
@@ -74,5 +74,6 @@ namespace DatingApp.API.Data
         return true;
       return false;
     }
+    
   }
 }
